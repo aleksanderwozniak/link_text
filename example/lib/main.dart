085091c0f06b4b3ce458b22bfbf1f6856db10c5a
Aleksander Woźniak <aleksanderwozniak96@gmail.com>
2019-07-13 22:34:48 +0200

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,87 @@
+//  Copyright (c) 2019 Aleksander Woźniak
+//  Licensed under Apache License v2.0
+
+import 'package:flutter/material.dart';
+import 'package:link_text/link_text.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LinkText Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'LinkText Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String _text = 'Lorem ipsum https://flutter.dev\nhttps://pub.dev dolor https://google.com sit amet';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 24.0),
+              _buildNormalText(),
+              const SizedBox(height: 64.0),
+              _buildLinkText(),
+              const SizedBox(height: 24.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNormalText() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(
+          'Normal Text Widget',
+          textAlign: TextAlign.center,
+          style: TextStyle().copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 12.0),
+        Text(_text, textAlign: TextAlign.center),
+      ],
+    );
+  }
+
+  Widget _buildLinkText() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(
+          'LinkText Widget',
+          textAlign: TextAlign.center,
+          style: TextStyle().copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 12.0),
+        LinkText(text: _text, textAlign: TextAlign.center),
+      ],
+    );
+  }
+}
---
+++ lib/link_text.dart
@@ -0,0 +1,94 @@
+//  Copyright (c) 2019 Aleksander Woźniak
+//  Licensed under Apache License v2.0
+
+library link_text;
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkText extends StatefulWidget {
+  final String text;
+  final TextStyle textStyle;
+  final TextStyle linkStyle;
+  final TextAlign textAlign;
+
+  const LinkText({
+    Key key,
+    @required this.text,
+    this.textStyle,
+    this.linkStyle,
+    this.textAlign = TextAlign.start,
+  })  : assert(text != null),
+        super(key: key);
+
+  @override
+  _LinkTextState createState() => _LinkTextState();
+}
+
+class _LinkTextState extends State<LinkText> {
+  List<TapGestureRecognizer> _gestureRecognizers;
+  final RegExp _regex =
+      RegExp(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
+
+  @override
+  void initState() {
+    super.initState();
+    _gestureRecognizers = <TapGestureRecognizer>[];
+  }
+
+  @override
+  void dispose() {
+    _gestureRecognizers.forEach((recognizer) => recognizer.dispose());
+    super.dispose();
+  }
+
+  void _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final textStyle = this.widget.textStyle ?? themeData.textTheme.body1;
+    final linkStyle = this.widget.linkStyle ??
+        themeData.textTheme.body1.copyWith(color: themeData.accentColor, decoration: TextDecoration.underline);
+
+    final links = _regex.allMatches(widget.text);
+
+    if (links.isEmpty) {
+      return Text(widget.text, style: textStyle);
+    }
+
+    final textParts = widget.text.split(_regex);
+    final textSpans = <TextSpan>[];
+
+    int i = 0;
+    textParts.forEach((part) {
+      textSpans.add(TextSpan(text: part, style: textStyle));
+
+      if (i < links.length) {
+        final link = links.elementAt(i).group(0);
+        final recognizer = TapGestureRecognizer()..onTap = () => _launchUrl(link);
+
+        _gestureRecognizers.add(recognizer);
+        textSpans.add(TextSpan(
+          text: link,
+          style: linkStyle,
+          recognizer: recognizer,
+        ));
+
+        i++;
+      }
+    });
+
+    return RichText(
+      text: TextSpan(children: textSpans),
+      textAlign: widget.textAlign,
+    );
+  }
+}
